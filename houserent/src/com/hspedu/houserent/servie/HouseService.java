a8abd102b91d264a91f155599e3d50dca380b575
yffy53 <1656724010@qq.com>
2025-03-11 23:31:58 +0800

    2025.3.11

--- houserent/src/com/hspedu/houserent/servie/HouseService.java
+++ houserent/src/com/hspedu/houserent/servie/HouseService.java
@@ -2,13 +2,35 @@ package com.hspedu.houserent.servie;
 
 import com.hspedu.houserent.domain.House;
 
+/**
+ * 定义House[]，保存House对象
+ * 1.响应HouseView的调用
+ * 2.完成对房屋信息的各种操作（增删改查c[create]r[read]u[update]d[delete]）
+ */
+
 public class HouseService {
 
     private House[] houses;  //保存House对象
-
+    private int houseNums = 1; //记录当前有多个房屋信息
+    private int idCounter = 0; //记录当前的id增长到哪个值
+    //构造器
     public HouseService(int size) {
         houses = new House[size];  //当创建HouseService对象，指定数组大小
         houses[0] = new House(1, "jack", "708", "吉阳区", 2000, "未出租");
+    }
+
+    //add方法，添加新对象，返回boolean
+    public boolean add(House newHouse) {
+        //判断是否还可以继续添加（暂不考虑数组扩容）
+        if (houseNums == houses.length) {//不能再添加
+            System.out.println("数组已满，不能再添加...");
+            return false;
+        }
+        //把newHouse对象加入houses，新增加了一个房屋
+        houses[houseNums++] = newHouse;
+        //id自增长，然后更新newHouse的id
+        newHouse.setId(++idCounter);
+        return true;
     }
 
     public House[] list() {
--- houserent/src/com/hspedu/houserent/view/HouseView.java
+++ houserent/src/com/hspedu/houserent/view/HouseView.java
@@ -1,21 +1,61 @@
 package com.hspedu.houserent.view;
 
 
+import com.hspedu.houserent.domain.House;
+import com.hspedu.houserent.servie.HouseService;
 import com.hspedu.houserent.utils.Utility;
 
 public class HouseView {
 
-    private boolean loop = true;
-    private char key;
+    private boolean loop = true; //控制显示菜单
+    private char key; //接收用户选择
+    private HouseService houseService = new HouseService(10);//设置数组大小
 
+    //编写delHouse() 接受输入的id，调用Service 的del方法
+    public  void delHouse() {
+        System.out.println("==============删除房屋信息==============");
+    }
+
+    //编写addHouse() 接收输入，创建House对象，调用add方法
+    public void addHouse() {
+        System.out.println("==============添加房屋==============");
+        System.out.print("姓名: ");
+        String name = Utility.readString(8);
+        System.out.print("电话: ");
+        String phone = Utility.readString(12);
+        System.out.print("地址: ");
+        String address = Utility.readString(16);
+        System.out.print("月租: ");
+        int rent = Utility.readInt();
+        System.out.print("状态（未出租/已出租）: ");
+        String state = Utility.readString(3);
+        //创建一个新的House对象，注意id是系统分配的
+        House newHouse = new House(0, name, phone, address, rent, state);
+        if (houseService.add(newHouse)) {
+            System.out.println("==============添加房屋成功==============");
+        } else {
+            System.out.println("==============添加房屋失败==============");
+        }
+    }
+
+    //编写listHouse()显示房屋列表
     public void listHouse() {
         System.out.println("==============房屋列表==============");
         System.out.println("编号\t\t房主\t\t电话\t\t地址\t\t月租\t\t状态（未出租/已出租）");
+        House[] houses = houseService.list();//得到所有房屋信息
+        for (House house : houses) {
+            if (house == null) {//null不用再显示
+                break;
+            }
+            System.out.println(house);
+        }
+        System.out.println("==============房屋列表显示完毕==============");
     }
 
+    //显示主菜单
     public void mainMenu() {
 
-        do{
+        do {
             System.out.println("==============房屋出租系统菜单==============");
             System.out.println("\t\t\t1 新 增 房 源");
             System.out.println("\t\t\t2 查 找 房 屋");
@@ -27,7 +67,7 @@ public class HouseView {
             key = Utility.readChar();
             switch (key) {
                 case '1':
-                    System.out.println("新增");
+                    addHouse();
                     break;
                 case '2':
                     System.out.println("查 找");
@@ -39,13 +79,13 @@ public class HouseView {
                     System.out.println("修 改");
                     break;
                 case '5':
-                    System.out.println("房 屋 列 表");
+                    listHouse();
                     break;
                 case '6':
                     System.out.println("退出");
                     loop = false;
                     break;
             }
-        }while(loop);
+        } while (loop);
     }
 }
